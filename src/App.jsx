efdb664e27daa5d9f43f29041f139ddf63693cf7
Anish Baraily <An2001.baraily@gmail.com>
2024-03-22 09:34:52 +0545

    added list

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Bottom from "./Components/Bottom/Bottom";
+import LeftMain from "./Components/LeftMain/LeftMain";
+import RightMain from "./Components/RightMain/RightMain";
+import Top from "./Components/Top/Top";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <Top />
+      <div className="content">
+        <LeftMain />
+        <RightMain />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Bottom/>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/Components/RightMain/RightMain.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./RightMain.css"
+
+function RightMain() {
+  return (
+    <div className='right-main'>
+      
+    </div>
+  )
+}
+
+export default RightMain
